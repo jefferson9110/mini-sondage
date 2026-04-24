6acb0e8021bf417bf7dbe2ce0b1aca6f11b09868
Jefferson <djomojefferson@gmail.com>
2026-04-24 13:51:09 +0100

    implémentation des modèles sans faire le makemigrations et migrate

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+#On ne crée pas ne table user car django le fait deja pour nous par l'imporation du 2e package
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Survey(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Response(models.Model):
+    choix = [('OUI', 'Oui'), ('NON', 'Non')]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    content = models.CharField(max_length=3, choices=choix)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'survey')  # Empêche un double vote
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.survey.title}"
+    
